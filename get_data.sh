b71d0339c0fea49ec066bd0191a0ec93f41eae7a
Sasha Fedchin <sasha.fedchin@gmail.com>
2024-07-27 09:31:37 +0200

    Minor tweaks to wikisource parsing

--- get_data.sh
+++ get_data.sh
@@ -8,6 +8,6 @@ curl -o ./frwikisource.xml.bz2 https://dumps.wikimedia.org/frwikisource/20240501
 bzip2 -d frwikisource.xml.bz2
 curl -o ./ruwikisource.xml.bz2 https://dumps.wikimedia.org/ruwikisource/20240501/ruwikisource-20240501-pages-meta-current.xml.bz2
 bzip2 -d ruwikisource.xml.bz2
-cd wikiextractor && python3 -m wikiextractor.WikiExtractor ../enwikisource.xml -o ../en/ -ns Author,Index,Category,Template,Portal,Wikisource,Module,MediaWiki,Help && cd ../
-cd wikiextractor && python3 -m wikiextractor.WikiExtractor ../ruwikisource.xml -o ../ru/ -ns Автор,Индекс,Участник,Обсуждение,Категория,Шаблон,Портал,Викитека,Модуль,Медивики,MediaWiki,Справка && cd ../
-cd wikiextractor && python3 -m wikiextractor.WikiExtractor ../frwikisource.xml -o ../fr/ -ns Auteur,Modèle,Wikisource,MediaWiki,Portail,Module,Aide,Catégorie,Discussion && cd ../
+python3 -m wikiextractor.wikiextractor.WikiExtractor enwikisource.xml -o en/ -ns Author,Index,Category,Template,Portal,Wikisource,Module,MediaWiki,Help
+python3 -m wikiextractor.wikiextractor.WikiExtractor ruwikisource.xml -o ru/ -ns Автор,Индекс,Участник,Обсуждение,Категория,Шаблон,Портал,Викитека,Модуль,Медивики,MediaWiki,Справка
+python3 -m wikiextractor.wikiextractor.WikiExtractor frwikisource.xml -o fr/ -ns Auteur,Modèle,Wikisource,MediaWiki,Portail,Module,Aide,Catégorie,Discussion
